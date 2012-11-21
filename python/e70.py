3f218c1e08dea90a262c159234825dd015815e1d
Miha Zidar <zidarsk8@gmail.com>
2012-11-21 02:08:16 +0100

    70 solved

--- python/e70.py
+++ python/e70.py
@@ -1,38 +1,6 @@
 import math
+from operator import mul
 
-def prime():
-    p,i = [2,3],3
-    yield 2
-    while 1:
-        yield i
-        while 1:
-            c,i,s= 0, i+2, math.sqrt(i+2)
-            while i % p[c] != 0 and p[c] <= s:
-                c += 1
-            if i % p[c] != 0:
-                p.append(i)
-                break
-
-
-def getDevs0(n):
-    nrt = int(math.sqrt(n))+1
-    primes = []
-    for p in prime():
-        primes.append(p)
-        if p > nrt:
-            break
-
-    count = 0
-    a = [0,1]
-    for i in xrange(2,n+1):
-        for p in primes:
-            if i%p == 0:
-                a.append(p)
-                break
-            if p > math.sqrt(i):
-                a.append(i)
-                break
-    return a
 
 def getDevs1(n):
     devs = range(n+1)
@@ -42,15 +10,27 @@ def getDevs1(n):
                 devs[j] = i if devs[j] == j else devs[j]
     return devs
 
-def getDevs2(n):
-    devs = range(n+1)
-    for i,j in  [(i,j) for i in xrange(2,int(math.sqrt(n))+1) if devs[i] == i for j in xrange(i,n+1,i)]:
-        devs[j] = i if devs[j] == j else devs[j]
-    return devs
+def getSingle(devs,i):
+    a=set()
+    while devs[i]>1:
+        a.add(devs[i])
+        i = i / devs[i]
+    return a
+
+def prod(n,p):
+    return int(round(n*reduce(mul,[1.-1./i for i in p])))
+
+def isPermut(a,b):
+    return sorted(list(str(a))) == sorted(list(str(b)))
+
+n = 10**7-1
+d = getDevs1(n)
+phi = [0,1]+[prod(i,getSingle(d,i)) for i in range(2,len(d))]
+
+pairs = sorted([(float(i)/phi[i],i,phi[i]) for i in range(2,len(phi)) if isPermut(i,phi[i])])
+
+print pairs[0]
 
 
-n = 1000000
-d = getDevs2(n)
 
-print sum(d)
 
